8fdc0a8319362529c904e541db45fdcb87693785
aravindcz <aravindcnair24@gmail.com>
2023-07-08 17:42:56 +0530

    feat: Use http library to make network request to pokedex api and unmarshal the response using encoding/json library

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"pokeapi"
 )
+
 
 var (
 	input string
+	response pokeapi.Response
 )
 
 func main() {
-
-
 
 	for{
 		fmt.Print("pokedex > ")
@@ -23,10 +24,12 @@ func main() {
 			help: prints a help message describing how to use the REPL
 			exit: exits the program`)
 
+		}else if input== "map" || input == "mapb"{
+			 response  = pokeapi.GetPokeApiResult(input)
+			 fmt.Println(string(response))
 		}else{
 			break
 		}
 	}
-	
 
 }
---
+++ pokeapi/pokeapi.go
@@ -0,0 +1,75 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+var (
+	config Config
+)
+
+	
+type Response struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string    `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+type Config struct{
+	Previous string
+	Next string
+}
+
+func GetPokeApiResult(command string) *Response{
+
+	var request string
+	var responseBodyUnmarshalled Response
+
+
+	if config.Previous == ""{
+		config.Next = "https://pokeapi.co/api/v2/location/?offset=0&limit=20"
+	}
+
+	if command == "map"{
+		request = config.Next
+	}else{
+		if config.Previous == "" || config.Previous == null{
+			return nil
+		}
+		request = config.Previous
+	}
+
+	response,err := http.Get(request)
+
+
+	if err != nil {
+		fmt.Println("Some error occured")
+		return nil
+	}
+
+	responseBody,err := io.ReadAll(response.Body)
+
+	if err!=nil{
+		return nil
+	}
+
+	err = json.Unmarshal(responseBody,&responseBodyUnmarshalled)
+
+	if err != nil{
+		return nil
+	}
+
+	config.Next = responseBodyUnmarshalled.Next
+	config.Previous = responseBodyUnmarshalled.Previous
+
+	return &responseBodyUnmarshalled
+
+}
+
